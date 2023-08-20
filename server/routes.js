a0661794dbce22563f8101cf57800fa33934bbc6
AbdulRehman <abdulrehman6126@gmail.com>
2023-08-20 23:09:10 +0500

    Initializing the backend + scaffolding

--- server/routes.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-
-router.get("/hello", (req, res)=> {
-return res.status (200).json({status: 200, message : " hello front server"});
-});
-
-
-module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,22 +1,24 @@
-const express = require ("express");
+const express = require('express');
 const cors = require('cors'); // Import the cors module
-const app = express()
-const PORT = 8000;
-const { API_PORT } = process.env;
-const http = require("http");
-const app = require("./app");
-const server = http.createServer(app);
-const port = process.env.PORT || API_PORT;
-app.use(cors()); // Allow all origins (for development; restrict in production)
-app.use(express.json ());
-app.use(require("./routes"));
+const app = express();
+const dotenv = require('dotenv');
 
+const PORT = process.env.PORT || 8000;
 
+// Load environment variables from .env
+dotenv.config();
 
+// Initialize the DB connection
+require('./src/configs/dbClient');
 
+// Allow all origins (for development; restrict in production)
+app.use(cors());
 
+app.use(express.json());
 
+// Import the API Routes - This includes the API paths
+app.use(require('./src/routes/routes'));
 
-app.listen(PORT, ()=> {
-console.log(`server listenning on port ${PORT}`);
-})
+app.listen(PORT, () => {
+	console.log(`server listenning on port ${PORT}`);
+});
