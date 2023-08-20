c9b655b917c97a82583a066df48043c9915dab69
AbdulRehman <abdulrehman6126@gmail.com>
2023-08-21 03:14:42 +0500

    Restructuring the frontend app

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,34 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Meals from "./Meals";
-import Order from './Order';
-import ConfirmationOrder from './ConfirmationOrder';
-import LandingPage from "./LandingPage";
-import NavBar from "./NavBar";
-import SignInForm from "./SignInForm";
-import MealsDetails from "./MealsDetails";
-import Drinks from "./Drinks";
-import DrinksDetails from "./DrinksDetails";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Meals from './pages/Meals';
+import Order from './pages/Order';
+import ConfirmationOrder from './pages/ConfirmationOrder';
+import LandingPage from './pages/LandingPage/LandingPage';
+import NavBar from './components/NavBar';
+import SignInForm from './pages/SignInForm';
+import MealsDetails from './pages/MealsDetails';
+import Drinks from './pages/Drinks';
+import DrinksDetails from './pages/DrinksDetails';
 
 const App = () => {
-  
-  return (
-    <BrowserRouter>
-    <NavBar/>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/meals" element={<Meals />} />
-        <Route path="/meal/:mealDetails" element={<MealsDetails />} />
-        <Route path="/Drinks" element={<Drinks />} />
-        <Route path="/Drinks/DrinksDetails" element={<DrinksDetails />} />
-        <Route path="/signin" element={<SignInForm />} />
-        <Route path="/order" element={<Order />} />
-        <Route path="/confirmationOrder/:orderId" element={<ConfirmationOrder />} /> 
-        <Route path="" element={<h1>404: Oops!</h1>} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
+	return (
+		<BrowserRouter>
+			<NavBar />
+			<Routes>
+				<Route path='/' element={<LandingPage />} />
+				<Route path='/meals' element={<Meals />} />
+				<Route path='/meal/:mealDetails' element={<MealsDetails />} />
+				<Route path='/Drinks' element={<Drinks />} />
+				<Route path='/Drinks/DrinksDetails' element={<DrinksDetails />} />
+				<Route path='/signin' element={<SignInForm />} />
+				<Route path='/order' element={<Order />} />
+				<Route
+					path='/confirmationOrder/:orderId'
+					element={<ConfirmationOrder />}
+				/>
+				<Route path='' element={<h1>404: Oops!</h1>} />
+			</Routes>
+		</BrowserRouter>
+	);
+};
 
 export default App;
-
---
+++ client/src/pages/LandingPage/LandingPage.js
@@ -0,0 +1,20 @@
+import './LandingPage.css';
+import styled from 'styled-components';
+
+const LandingPage = () => {
+	return (
+		<Wrapper>
+			<div className='landing-container'>
+				<h1 className='title'>Welcome to Food Eats</h1>
+				<p className='subtitle'>Discover our Fresh meals Today</p>
+				<a href='/FoodList' className='get-started-button'>
+					Get Started
+				</a>
+			</div>
+		</Wrapper>
+	);
+};
+
+const Wrapper = styled.div``;
+
+export default LandingPage;
