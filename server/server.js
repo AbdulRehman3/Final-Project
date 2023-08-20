2ab3ad22b663ea4f1bae593cf41d4dbb87d9f05c
AbdulRehman <abdulrehman6126@gmail.com>
2023-08-20 23:23:59 +0500

    Minor fixes for server

--- server/server.js
+++ server/server.js
@@ -3,21 +3,22 @@ const cors = require('cors'); // Import the cors module
 const app = express();
 const dotenv = require('dotenv');
 
-const PORT = process.env.PORT || 8000;
-
 // Load environment variables from .env
 dotenv.config();
+const PORT = process.env.PORT || 8000;
 
 // Initialize the DB connection
 require('./src/configs/dbClient');
+
+const apiRoutes = require('./src/routes/routes');
 
 // Allow all origins (for development; restrict in production)
 app.use(cors());
 
 app.use(express.json());
 
-// Import the API Routes - This includes the API paths
-app.use(require('./src/routes/routes'));
+// Import the API Routes - this includes all the API paths
+app.use('/api', apiRoutes);
 
 app.listen(PORT, () => {
 	console.log(`server listenning on port ${PORT}`);
